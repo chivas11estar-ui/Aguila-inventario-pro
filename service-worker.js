bfc2111b45632611a4d654388e15a7d9cb7e104d
chivas11estar-ui <chivas11estar@gmail.com>
2025-11-09 11:08:40 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ============================================================
 // Águila Inventario Pro - Service Worker
 // Copyright © 2025 José A. G. Betancourt
-// VERSIÓN 7.7 - CORREGIDA Y COMPLETA
+// VERSIÓN 7.6 - CORREGIDA Y COMPLETA
 // ============================================================
 
 // CAMBIO 1: Nuevo nombre de caché para forzar la actualización
@@ -105,6 +105,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
 
 console.log("✅ service-worker.js cargado correctamente");
