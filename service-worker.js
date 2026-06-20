419c468c0df70f4bc5481fcb77f66b6b82b3b1c1
chivas11estar-ui <chivas11estar@gmail.com>
2026-06-19 22:37:57 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Estrategia: stale-while-revalidate para el App Shell
 // ============================================================
 
-const CACHE_NAME = "aguila-pro-v5.2";
+const CACHE_NAME = "aguila-pro-v5.3";
 
 const APP_SHELL_ASSETS = [
   "/",
