68c27537be47a4c77bcb427ed9df7aebeb9ed086
chivas11estar-ui <chivas11estar@gmail.com>
2026-06-23 21:08:10 -0600

    chore: renovar caché visual

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Estrategia: stale-while-revalidate para el App Shell
 // ============================================================
 
-const CACHE_NAME = "aguila-pro-v5.5";
+const CACHE_NAME = "aguila-pro-v5.6";
 
 const APP_SHELL_ASSETS = [
   "/",
